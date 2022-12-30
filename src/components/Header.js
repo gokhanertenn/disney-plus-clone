c57298a4a402d09691c7c4333f946690cebecd98
gokhan <gokhanerten31@icloud.com>
2022-12-31 01:38:27 +0200

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src = "/images/logo.svg"/>
+        
+       <NavMenu>
+        <a>
+            <img src="/images/home-icon.svg"/>
+            <span>HOME</span>
+
+            
+        </a>
+        <a>
+            <img src="/images/search-icon.svg"/>
+            <span>SEARCH</span>
+
+            
+        </a>
+        <a>
+            <img src="/images/watchlist-icon.svg"/>
+            <span>WATCHLİST</span>
+
+            
+        </a>
+        <a>
+            <img src="/images/original-icon.svg"/>
+            <span>ORİGİNALS</span>
+
+            
+        </a>
+        <a>
+            <img src="/images/movie-icon.svg"/>
+            <span>MOVİES</span>
+
+            
+        </a>
+        <a>
+            <img src="/images/series-icon.svg"/>
+            <span>SERİES</span>
+
+            
+        </a>
+       </NavMenu> 
+
+    </Nav>
+  )
+}
+
+export default Header
+Header
+
+const Nav = styled.div `
+height:70px;
+background:#090b13;
+display:flex;
+align-items:center;
+padding: 0 36px;
+
+`
+
+const Logo = styled.img`
+width:80px;
+
+
+`
+
+const NavMenu = styled.div`
+display:flex;
+
+  a {
+    display:flex;
+    align-items:center;
+    padding:0 12px;
+
+    img {
+        height:20px
+    }
+
+    span {
+        font-size:13px;
+        letter-spacing:1.42px;
+    }
+  }
+
+`
